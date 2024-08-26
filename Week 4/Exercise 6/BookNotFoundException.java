7b779bc4c7662ce6a4984d3b0b9373ac68b35a22
Bharathkumar0052 <123620907+Bharathkumar0052@users.noreply.github.com>
2024-08-26 17:43:38 +0530

    Delete Week 4 directory

--- Week 4/Exercise 6/BookNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.bookstoreapi.exception;
-
-public class BookNotFoundException extends RuntimeException {
-
-    public BookNotFoundException(String message) {
-        super(message);
-    }
-}
